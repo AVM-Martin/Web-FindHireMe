d950394515d07391c3c191dbcdeeb9528e25c258
ekeitaro <edwardkeitaro.heru@gmail.com>
2020-12-30 14:12:54 +0700

    script + template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>
+    @section('title')
+      {{ config('app.name', 'Laravel') }}
+    @show
+  </title>
+</head>
+
+<script src="{{ asset('js/jquery-3.4.1.slim.min.js') }}" crossorigin="anonymous"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}" crossorigin="anonymous"></script>
+<script src="{{ asset('js/app.js') }}" crossorigin="anonymous"></script>
+
+<body>
+
+</body>
+</html>
